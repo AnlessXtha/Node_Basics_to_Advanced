65ab546f148cd43b86bed52632f3c04f4943b853
AnlessXtha <aadesh.shrrstha@gmail.com>
2023-12-13 14:41:32 +0545

    added the math example(custom queries)

--- Chapter-1/index.js
+++ Chapter-1/index.js
@@ -12,10 +12,12 @@ console.log("Hello World");
 
 // 4) Has Common Core modules that we will explore (relating to the operating system/ file system)
 // 5) CommonJS module instead of its modules(uses require)
+// 6) Missing some JS APIs like fetch.(we can use various packages so we can use them instead)
 
 const os = require("os");
 const path = require("path");
 
+/** 
 console.log(os.type()); // displays the windows type
 console.log(os.version()); // displays the windows version
 console.log(os.homedir()); // displays the windows home directory
@@ -29,3 +31,18 @@ console.log(path.extname(__filename)); // Displays the extension of the current 
 
 console.log(path.parse(__filename));
 // Displays an object with various value such as root, dir, base, ext, name.
+*/
+
+// Custom Module
+
+// const math = require("./math");
+
+// console.log(math.add(2, 3));
+
+// Destructuring math module
+const { add, substract, multiply, divide } = require("./math");
+
+console.log(add(2, 3));
+console.log(substract(2, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 3));
